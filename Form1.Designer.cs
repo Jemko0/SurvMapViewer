677aef9c8680ca1382789beebd114ff9839f9d69
Jemko <jemko23@t-online.de>
2024-03-27 02:56:12 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace SurvMapViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ZoomUp = new System.Windows.Forms.Button();
+            this.ZoomDown = new System.Windows.Forms.Button();
+            this.bgThread = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(26, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(783, 800);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ZoomUp
+            // 
+            this.ZoomUp.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.ZoomUp.Location = new System.Drawing.Point(851, 45);
+            this.ZoomUp.Name = "ZoomUp";
+            this.ZoomUp.Size = new System.Drawing.Size(261, 42);
+            this.ZoomUp.TabIndex = 1;
+            this.ZoomUp.Text = "Zoom [+]";
+            this.ZoomUp.UseVisualStyleBackColor = true;
+            this.ZoomUp.Click += new System.EventHandler(this.ZoomUp_Click);
+            // 
+            // ZoomDown
+            // 
+            this.ZoomDown.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.ZoomDown.Location = new System.Drawing.Point(851, 93);
+            this.ZoomDown.Name = "ZoomDown";
+            this.ZoomDown.Size = new System.Drawing.Size(261, 42);
+            this.ZoomDown.TabIndex = 2;
+            this.ZoomDown.Text = "Zoom [-]";
+            this.ZoomDown.UseVisualStyleBackColor = true;
+            this.ZoomDown.Click += new System.EventHandler(this.ZoomDown_Click);
+            // 
+            // bgThread
+            // 
+            this.bgThread.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgThread_DoWork);
+            this.bgThread.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgThread_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1141, 859);
+            this.Controls.Add(this.ZoomDown);
+            this.Controls.Add(this.ZoomUp);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Surv Map Viewer (highly intelligent)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button ZoomUp;
+        private System.Windows.Forms.Button ZoomDown;
+        public System.ComponentModel.BackgroundWorker bgThread;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,290 @@
+﻿using FastNoise;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Design;
+using System.Drawing.Text;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace SurvMapViewer
+{
+    public partial class Form1 : Form
+    {
+        //configurable
+        public static int WorldSeed = -617053759;
+        
+        public static int imgsize = 500;
+
+        //MapControl
+        public float ZoomFactor = 1f;
+        public float ViewX = 0;
+        public float ViewY = 0;
+        //INGAME UNCHANGED VARS
+        public float NoiseScale = 375f;
+        public float ZMultiplier = 11500.0f;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Noise Initialization
+        FastNoise.FastNoise MainNoise = new FastNoise.FastNoise();
+        FastNoise.FastNoise ErosionNoise = new FastNoise.FastNoise();
+        FastNoise.FastNoise HillsNoise = new FastNoise.FastNoise();
+        FastNoise.FastNoise PeaksNoise = new FastNoise.FastNoise();
+        FastNoise.FastNoise OceanNoise = new FastNoise.FastNoise();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetupNoise();
+            DrawMapAsync();
+        }
+        public Bitmap bmp = new Bitmap(imgsize, imgsize);
+        public bool graphicsLock = false;
+        private void DrawMapAsync()
+        {
+            /* Mult thread approach 1
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                for (int xit = 0; xit < imgsize; xit++)
+                {
+                    for (int yit = 0; yit < imgsize; yit++)
+                    {
+                        float nvalraw = GetNoise(xit * ZoomFactor, yit * ZoomFactor);
+                        float nval = nvalraw / 5000000;
+                        if (!graphicsLock)
+                        {
+                            bmp.SetPixel(xit, yit, GetMapColor(nval));
+                        }
+                    }
+                }
+                
+            }).Start();
+            pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(pictureBox1_Paint);
+            */
+
+            //Mult thread approach 2
+            bgThread.RunWorkerAsync();
+        }
+
+        private void bgThread_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int xit = 0; xit < imgsize; xit++)
+            {
+                for (int yit = 0; yit < imgsize; yit++)
+                {
+                    float valraw = GetNoise(xit * ZoomFactor, yit * ZoomFactor);
+                    float val = valraw / 5000000;
+                    bmp.SetPixel(xit, yit, GetMapColor(val));
+                }
+            }
+        }
+
+        private void bgThread_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(pictureBox1_Paint);
+            //so fresh and clean
+            pictureBox1.Refresh();
+        }
+
+        /*
+        private void DrawMap()
+        {
+            
+            pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(pictureBox1_Paint);
+            
+            for (int xit = 0; xit < imgsize; xit++)
+            {
+                for (int yit = 0; yit < imgsize; yit++)
+                {
+                    float nvalraw = GetNoise(xit * ZoomFactor, yit * ZoomFactor);
+                    float nval = nvalraw / 5000000;
+                    bmp.SetPixel(xit, yit, GetMapColor(nval));
+                }
+            }
+            pictureBox1.Refresh();
+        }
+        */
+
+        private Color GetMapColor(float noiseValue)
+        {
+            //throw new Exception("NoiseValue is" + noiseValue.ToString());
+            Color Sand = Color.Yellow;
+            Color Grass = Color.FromArgb((int)noiseValue, 0, 255, 0);
+            Color Water = Color.FromArgb((int)noiseValue, 0, 0, 255);
+
+            if(noiseValue > 201)
+            {
+                return Grass;
+            }
+            else
+            {
+                return Water;
+            }
+        }
+        private void pictureBox1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            graphicsLock = true;
+            Graphics g = e.Graphics;
+            e.Graphics.DrawImage(bmp, 0, 0);
+            graphicsLock = false;
+        }
+
+        private void SetupNoise()
+        {
+            //LAYER 1
+            MainNoise.SetNoiseType(FastNoise.FastNoise.NoiseType.SimplexFractal);
+            MainNoise.SetSeed(Util.TruncFloatToInt(WorldSeed - 23465 * 16.253735f));
+            MainNoise.SetFrequency(0.0012f * (NoiseScale / 750));
+            MainNoise.SetFractalOctaves(3);
+            MainNoise.SetFractalLacunarity(1.85f);
+            MainNoise.SetCellularJitter(0.35f);
+            MainNoise.SetFractalGain(0.45f);
+            MainNoise.SetInterp(FastNoise.FastNoise.Interp.Quintic);
+            MainNoise.SetFractalType(FastNoise.FastNoise.FractalType.RigidMulti);
+            MainNoise.SetCellularDistanceFunction(FastNoise.FastNoise.CellularDistanceFunction.Natural);
+
+            //LAYER 2
+            ErosionNoise.SetNoiseType(FastNoise.FastNoise.NoiseType.SimplexFractal);
+            ErosionNoise.SetSeed(WorldSeed);
+            ErosionNoise.SetFrequency(0.0012f * (NoiseScale / 750));
+            ErosionNoise.SetFractalOctaves(9);
+            ErosionNoise.SetFractalLacunarity(2f);
+            ErosionNoise.SetCellularJitter(0.45f);
+            ErosionNoise.SetFractalGain(0.65f);
+            ErosionNoise.SetInterp(FastNoise.FastNoise.Interp.Quintic);
+            ErosionNoise.SetFractalType(FastNoise.FastNoise.FractalType.FBM);
+            ErosionNoise.SetCellularDistanceFunction(FastNoise.FastNoise.CellularDistanceFunction.Natural);
+
+            //LAYER 3
+            HillsNoise.SetNoiseType(FastNoise.FastNoise.NoiseType.ValueFractal);
+            HillsNoise.SetSeed(Util.TruncFloatToInt(WorldSeed - 23465 * 16.253735f));
+            HillsNoise.SetFrequency(0.01f * (NoiseScale / 750));
+            HillsNoise.SetFractalOctaves(6);
+            HillsNoise.SetFractalLacunarity(2f);
+            HillsNoise.SetCellularJitter(0.45f);
+            HillsNoise.SetFractalGain(0.5f);
+            HillsNoise.SetInterp(FastNoise.FastNoise.Interp.Quintic);
+            HillsNoise.SetFractalType(FastNoise.FastNoise.FractalType.FBM);
+            HillsNoise.SetCellularDistanceFunction(FastNoise.FastNoise.CellularDistanceFunction.Natural);
+
+            //LAYER 4
+            PeaksNoise.SetNoiseType(FastNoise.FastNoise.NoiseType.Cubic);
+            PeaksNoise.SetSeed(WorldSeed);
+            PeaksNoise.SetFrequency(0.021f * (NoiseScale / 750));
+            PeaksNoise.SetFractalOctaves(2);
+            PeaksNoise.SetFractalLacunarity(2f);
+            PeaksNoise.SetCellularJitter(1f);
+            PeaksNoise.SetFractalGain(0.5f);
+            PeaksNoise.SetInterp(FastNoise.FastNoise.Interp.Quintic);
+            PeaksNoise.SetFractalType(FastNoise.FastNoise.FractalType.Billow);
+            PeaksNoise.SetCellularDistanceFunction(FastNoise.FastNoise.CellularDistanceFunction.Natural);
+
+            //LAYER 5
+            OceanNoise.SetNoiseType(FastNoise.FastNoise.NoiseType.Perlin);
+            OceanNoise.SetSeed(WorldSeed + 13214 / 4);
+            OceanNoise.SetFrequency(0.0018f * (NoiseScale / 750));
+            OceanNoise.SetFractalOctaves(2);
+            OceanNoise.SetFractalLacunarity(2f);
+            OceanNoise.SetCellularJitter(0.45f);
+            OceanNoise.SetFractalGain(0.65f);
+            OceanNoise.SetInterp(FastNoise.FastNoise.Interp.Quintic);
+            OceanNoise.SetFractalType(FastNoise.FastNoise.FractalType.FBM);
+            OceanNoise.SetCellularDistanceFunction(FastNoise.FastNoise.CellularDistanceFunction.Natural);
+        }
+
+        private float GetNoise(float x, float y)
+        {
+            float mn_height;
+            float er_height;
+            float hi_height;
+            float pe_height;
+            float oc_height;
+            float finalheight;
+
+            mn_height = MainNoise.GetNoise(x, y) * ZMultiplier;
+            er_height = (ErosionNoise.GetNoise(x, y) * 1.5f) * 4000f;
+            hi_height = Util.Clamp(HillsNoise.GetNoise(x, y) * ZMultiplier, 0, 999999999);
+            pe_height = Util.Clamp(PeaksNoise.GetNoise(x, y), 0, 1) * Util.Clamp((11500 * 4 - (MainNoise.GetNoise(x, y) * 32)), 0, 99999999);
+            oc_height = Util.Clamp(OceanNoise.GetNoise(x, y), 0, 0.5f);
+
+            finalheight = ((er_height - mn_height) + hi_height + (pe_height * er_height / 1000) * oc_height);
+
+            return finalheight;
+        }
+
+        private void ZoomUp_Click(object sender, EventArgs e)
+        {
+            ZoomFactor = ZoomFactor + 0.1f;
+            DrawMapAsync();
+        }
+
+        private void ZoomDown_Click(object sender, EventArgs e)
+        {
+            ZoomFactor = ZoomFactor - 0.1f;
+            DrawMapAsync();
+        }
+
+
+        //other shit classes for TYPE CONVERSION because fuck you
+    }
+    public class Util
+    {
+        public Util()
+        {
+            return;
+        }
+
+        public static int TruncFloatToInt(float f)
+        {
+            return (int)f;
+        }
+
+        public static int TruncDoubleToInt(double d)
+        {
+            return (int)d;
+        }
+
+        public static T Clamp<T>(T value, T max, T min)
+        where T : System.IComparable<T>
+        {
+            T result = value;
+            if (value.CompareTo(max) > 0)
+                result = max;
+            if (value.CompareTo(min) < 0)
+                result = min;
+            return result;
+        }
+    }
+
+    public class baseConv
+        {
+            public static float baseIntFloat(int i)
+             {
+                return (float)i;
+             }
+         public static int baseFloatInt(int f)
+            {
+                return (int)f;
+            }
+
+        public static int baseDoubleInt(double d)
+        {
+            return (int)d;
+        }
+    }
+
+    }
